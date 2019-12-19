053fb51681265ce7c6b989cf910bb3d6695fbd62
Your Name <you@example.com>
2019-12-19 09:22:37 +0100

    function, expression

--- bash/z7.sh
+++ bash/z7.sh
@@ -20,3 +20,22 @@ while [ $x -le 10 ]; do
     Moja
 x=$[x + 1]
 done
+
+#wyrażenia arytmetryczne
+echo $((2+2))
+echo $[2+2]
+
+x=5
+y=2
+
+let suma=${x}+${y}
+echo ${suma}
+
+#operatowy arytmetryczne
+# + : dodawanie
+# - : odejmowanie
+# * : mnożenie
+# / : dzielenie
+# % : reszta z dzielenia
+
+echo $[x%y]
