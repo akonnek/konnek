b5e2bb6490ea9adaf0967833182e76923413118e
Your Name <you@example.com>
2020-01-09 09:40:38 +0100

    showUsers, addUsers

---
+++ bash/manage_user.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+
+#zmienna globalna
+#ładowanie danych z pliku
+user_list=(`cat users.txt`)
+
+function showUsers() {
+    echo "showUsers ..."
+    echo "Lista:"
+    for(( i+=0; i<=${#user_list[@]}; i++ ))
+    do
+	echo "	${user_list[i]}"
+    done
+}
+function addUsers() {
+    echo "addUsers ..."
+    for user in "${user_list[@] }"
+    do
+	echo "Add user: ${user}"
+	useradd ${users} -s /sbin/nologin -g "users"
+    done
+}
+function delUsers() {
+    echo "delUsers ..."
+}
+function acceptRemoteLogin() {
+    echo "acceptRemoteLogin ..."
+}
+
+function deniedRemoteLogin() {
+    echo "deniedRemoteLogin ..."
+}
+function quit () {
+    exit 0
+}
+function help () {
+cat << EndOfMessage
+    Opis opcji skryptu:
+    SU -> Listowanie użytkowników z pliku
+    AU -> Dodawanie użytkowników
+    
+EndOfMessage
+}
+
+#menu
+select option in SU AU DU ARL DRL HELP QUIT
+do
+    case ${option} in
+	"SU") showUsers ;;
+	"AU") addUsers ;;
+	"DU") delUsers ;;
+	"ARL") acceptRemoteLogin ;;
+	"DRL") deniedRemoteLogin ;;
+	"HELP") help ;;
+	"QUIT") quit ;;
+	    *) help
+    esac
+done
