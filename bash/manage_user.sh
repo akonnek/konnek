5f18098b7c0688f4b969f46a4928c7f112e699c9
Your Name <you@example.com>
2020-01-15 12:34:06 +0100

    acceptRemoteLogin

--- bash/manage_user.sh
+++ bash/manage_user.sh
@@ -15,21 +15,44 @@ function showUsers() {
 }
 function addUsers() {
     echo "addUsers ..."
-    for user in "${user_list[@] }"
-    do
-	echo "Add user: ${user}"
-	useradd ${users} -s /sbin/nologin -g "users"
-    done
+    echo -n "Are you sure? [y/n]"
+    read sure
+    if [ "${sure}" == "y" ]; then
+	for user in "${user_list[@] }"
+	do
+	    echo "Add user: ${user}[OK]"
+	    sudo useradd ${user} -s /sbin/nologin -g "users"
+	done
+    fi
 }
 function delUsers() {
     echo "delUsers ..."
+    echo -n "Are you sure? [y/n]" 
+    read sure
+    if [ ${sure} == "y" ]; then
+	for user in "${user_list[@]}"
+	do
+	    echo "Remove user ${user} [OK]"
+	    sudo userdel ${user}
+	done
+    fi
 }
 function acceptRemoteLogin() {
     echo "acceptRemoteLogin ..."
+    for user in "${user_list[@] }"
+    do
+        echo "Accept remote login for ${user} [OK]"
+        sudo usermod -s /bin/bash ${user}
+    done
 }
 
 function deniedRemoteLogin() {
     echo "deniedRemoteLogin ..."
+    for user in "${user_list[@] }"
+    do
+        echo "Denied remote login for ${user} [OK]"
+        sudo usermod -s /sbin/nologin ${user}
+    done
 }
 function quit () {
     exit 0
@@ -39,6 +62,7 @@ cat << EndOfMessage
     Opis opcji skryptu:
     SU -> Listowanie użytkowników z pliku
     AU -> Dodawanie użytkowników
+    DU -> Usuwanie użytkowników
     
 EndOfMessage
 }
